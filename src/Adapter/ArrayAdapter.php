8372100b1a21d00ab0c26bac494474e1a0b39607
adlawson <adlawson@gmail.com>
2014-10-21 17:16:09 +0100

    Add Array Adapter implementation

---
+++ src/Adapter/ArrayAdapter.php
@@ -0,0 +1,57 @@
+<?php
+namespace Graze\Queue\Adapter;
+
+use ArrayIterator;
+use Graze\Queue\Message\MessageFactoryInterface;
+use Graze\Queue\Message\MessageInterface;
+use LimitIterator;
+
+class ArrayAdapter implements AdapterInterface
+{
+    /**
+     * @param MessageInterface[]
+     */
+    protected $queue;
+
+    /**
+     * @param MessageInterface[] $messages
+     */
+    public function __construct(array $messages = [])
+    {
+        $this->enqueue($messages);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function acknowledge(array $messages)
+    {
+        $this->queue = array_values(array_diff($this->queue, $messages));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function dequeue(MessageFactoryInterface $factory, $limit)
+    {
+        return new LimitIterator(new ArrayIterator($this->queue), 0, $limit);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function enqueue(array $messages)
+    {
+        foreach ($messages as $message) {
+            $this->addMessage($message);
+        }
+    }
+
+    /**
+     * @param MessageInterface $message
+     */
+    protected function addMessage(MessageInterface $message)
+    {
+        $this->queue[] = $message;
+    }
+}
---
+++ test/unit/Adapter/ArrayAdapterTest.php
@@ -0,0 +1,62 @@
+<?php
+namespace Graze\Queue\Adapter;
+
+use Mockery as m;
+use PHPUnit_Framework_TestCase as TestCase;
+
+class ArrayAdapterTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->factory = m::mock('Graze\Queue\Message\MessageFactoryInterface');
+
+        $this->messageA = $a = m::mock('Graze\Queue\Message\MessageInterface');
+        $this->messageB = $b = m::mock('Graze\Queue\Message\MessageInterface');
+        $this->messageC = $c = m::mock('Graze\Queue\Message\MessageInterface');
+        $this->messages = [$a, $b, $c];
+
+        $this->adapter = new ArrayAdapter($this->messages);
+    }
+
+    public function testInterface()
+    {
+        $this->assertInstanceOf('Graze\Queue\Adapter\AdapterInterface', $this->adapter);
+    }
+
+    public function testAcknowledge()
+    {
+        $this->adapter->acknowledge($this->messages);
+
+        $this->assertEquals([], iterator_to_array($this->adapter->dequeue($this->factory, 10)));
+    }
+
+    public function testAcknowledgeOne()
+    {
+        $this->adapter->acknowledge([$this->messageB]);
+
+        $this->assertEquals([$this->messageA, $this->messageC], iterator_to_array($this->adapter->dequeue($this->factory, 10)));
+    }
+
+    public function testDequeue()
+    {
+        $this->assertEquals($this->messages, iterator_to_array($this->adapter->dequeue($this->factory, 10)));
+    }
+
+    public function testDequeueWithLimit()
+    {
+        $this->assertEquals([$this->messageA], iterator_to_array($this->adapter->dequeue($this->factory, 1)));
+    }
+
+    public function testEnqueue()
+    {
+        $messageA = m::mock('Graze\Queue\Message\MessageInterface');
+        $messageB = m::mock('Graze\Queue\Message\MessageInterface');
+        $messageC = m::mock('Graze\Queue\Message\MessageInterface');
+        $messages = [$messageA, $messageB, $messageC];
+        $merged = array_merge($this->messages, $messages);
+
+        $this->adapter->enqueue($messages);
+
+        $this->assertEquals($merged, iterator_to_array($this->adapter->dequeue($this->factory, 10)));
+    }
+}
