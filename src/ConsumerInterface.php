a11d0ebc0ad6958b29f87516c0b8200b8fbff468
adlawson <adlawson@gmail.com>
2014-11-11 14:43:06 +0000

    Update consumer interface docblocks

--- src/ConsumerInterface.php
+++ src/ConsumerInterface.php
@@ -7,7 +7,7 @@ interface ConsumerInterface
 {
     /**
      * @param callable $worker
-     * @param integer $limit
+     * @param integer|null $limit Integer limit or Null no limit
      */
     public function receive(callable $worker, $limit = 1);
 }
