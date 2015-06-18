8e85b1654b5939971f123f40f09177b304596d71
Joe Meehan <joe.meehan@graze.com>
2015-06-18 08:37:14 +0100

    Update test cases

--- tests/integration/SqsIntegrationTest.php
+++ tests/integration/SqsIntegrationTest.php
@@ -67,7 +67,7 @@ class SqsIntegrationTest extends TestCase
         $this->sqsClient->shouldReceive('receiveMessage')->once()->with([
             'QueueUrl' => $url,
             'AttributeNames' => ['All'],
-            'MaxNumberOfMessages' => SqsAdapter::BATCHSIZE_RECEIVE,
+            'MaxNumberOfMessages' => 1,
             'VisibilityTimeout' => $timeout
         ])->andReturn($receiveModel);
 
@@ -81,6 +81,38 @@ class SqsIntegrationTest extends TestCase
         $msgs = [];
         $this->client->receive(function ($msg) use (&$msgs) {
             $msgs[] = $msg;
+        });
+
+        $this->assertCount(1, $msgs);
+    }
+
+    public function testReceiveWithLimit()
+    {
+        $url = $this->stubCreateQueue();
+        $timeout = $this->stubQueueVisibilityTimeout($url);
+
+        $receiveModel = m::mock('Guzzle\Service\Resource\Model');
+        $receiveModel->shouldReceive('getPath')->once()->with('Messages')->andReturn([
+            ['Body'=>'foo', 'Attributes'=>[], 'MessageAttributes'=>[], 'MessageId'=>0, 'ReceiptHandle'=>'a']
+        ]);
+        $this->sqsClient->shouldReceive('receiveMessage')->between(1, 100)->with([
+            'QueueUrl' => $url,
+            'AttributeNames' => ['All'],
+            'MaxNumberOfMessages' => SqsAdapter::BATCHSIZE_RECEIVE,
+            'VisibilityTimeout' => $timeout
+        ])->andReturn($receiveModel);
+
+        $deleteModel = m::mock('Guzzle\Service\Resource\Model');
+        $deleteModel->shouldReceive('getPath')->once()->with('Failed')->andReturn([]);
+        $this->sqsClient->shouldReceive('deleteMessageBatch')->once()->with([
+            'QueueUrl' => $url,
+            'Entries' => [['Id'=>0, 'ReceiptHandle'=>'a']]
+        ])->andReturn($deleteModel);
+
+        $msgs = [];
+        $this->client->receive(function ($msg, $done) use (&$msgs) {
+            $msgs[] = $msg;
+            $done();
         }, 100);
 
         $this->assertCount(1, $msgs);
